2854b042136b02b70f2669b40a234237c0d0b82c
Roman Khilchenko <extrimelife@hotmail.com>
2022-10-21 21:21:35 +0300

    Initial Commit

---
+++ ColorizeAppSwiftUI/ColorizeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizeAppSwiftUIApp.swift
+//  ColorizeAppSwiftUI
+//
+//  Created by roman Khilchenko on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
