c642a82b72be8409c87b1bcfc97bca0389369d82
Roman Khilchenko <extrimelife@hotmail.com>
2022-10-23 17:20:51 +0300

    Add TextField

--- ColorizeAppSwiftUI/SliderView.swift
+++ ColorizeAppSwiftUI/SliderView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SliderView: View {
     
     @Binding var value: Double
+    @State private var text = ""
     let color: Color
     
     var body: some View {
@@ -18,6 +19,14 @@ struct SliderView: View {
             Slider(value: $value, in: 0...250)
                 .frame(width: 250)
                 .accentColor(color)
+                .onChange(of: value) { newValue in
+                    text = "\(lroundl(Float80(newValue)))"
+                }
+            TextFieldView(text: $text, value: $value)
+        }
+        
+        .onAppear() {
+            text = "\(lroundl(Float80(value)))"
         }
     }
 }
---
+++ ColorizeAppSwiftUI/TextFieldView.swift
@@ -0,0 +1,28 @@
+//
+//  TextFieldView.swift
+//  ColorizeAppSwiftUI
+//
+//  Created by roman Khilchenko on 23.10.2022.
+//
+
+import SwiftUI
+
+struct TextFieldView: View {
+    
+    @Binding var text: String
+    @Binding var value: Double
+    
+    var body: some View {
+        TextField("", text: $text)
+            .frame(width: 55, alignment: .trailing)
+            .multilineTextAlignment(.trailing)
+            .textFieldStyle(.roundedBorder)
+            .keyboardType(.decimalPad)
+    }
+}
+
+struct TextFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldView(text: .constant("100"), value: .constant(100))
+    }
+}
